6caaf893715b29bac1444dc350a0d8c6d5a3a9ee
Alexandra Hedman <hedman.alexandra@gmail.com>
2022-05-18 14:29:46 +0200

    pushing rails yelp mvp

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,39 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit]
+
+  # GET /restaurants
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  # GET /restaurants/1
+  def show
+  end
+
+  # GET /restaurants/new
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  # POST /restaurants
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to @restaurant, notice: 'Restaurant was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_restaurant
+      @restaurant = Restaurant.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def restaurant_params
+      params.require(:restaurant).permit(:name, :address, :category)
+    end
+end
